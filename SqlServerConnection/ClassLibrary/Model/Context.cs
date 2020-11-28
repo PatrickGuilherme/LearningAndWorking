25648de1fc2945501c72b27d26bbeb1a13f9f8e2
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2020-11-28 02:34:34 -0300

    Adição de projeto de conexão com banco de dados
    Conexão com sql server

---
+++ SqlServerConnection/ClassLibrary/Model/Context.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ClassLibrary
+{
+    public class Context : DbContext
+    {
+        //Atributos ligados ao banco de dados
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Student> Student { get; set; }
+
+        /// <summary>
+        /// Inicia a criação do banco se não existir o banco
+        /// </summary>
+        public void StartConnection()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        /// <summary>
+        ///String de conexão
+        /// </summary>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Conexão utilizando sql server
+            optionsBuilder.UseSqlServer(
+                //Nome da conexão da maquina
+                "Data Source=DESKTOP-O9CABQT\\SQLEXPRESS;" +
+                //Nome do banco
+                "Initial Catalog=dbSqlConnection;" +
+                //Credenciais da conta atual do Windows são usadas para autenticação
+                "Integrated Security=True");
+        }
+
+        /// <summary>
+        /// Iniciar Configurações das tabelas
+        /// </summary>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ConfigDbCourse(modelBuilder);
+            ConfigDbStudent(modelBuilder);
+            ConfigDbStudentToCourse(modelBuilder);
+        }
+
+        /// <summary>
+        /// Configurações da tabela Course
+        /// </summary>
+        public void ConfigDbCourse(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>(etd =>
+            {
+                //Nome da tabela
+                etd.ToTable("tbCourse");
+                //Nome da coluna de id
+                etd.HasKey(p => p.CourseId).HasName("CourseId");
+                //Definição do tipo do campo e o autoincremento
+                etd.Property(p => p.CourseId).HasColumnType("int").ValueGeneratedOnAdd();
+                //Definição de campo not null e tamanho maximo
+                etd.Property(p => p.Name).IsRequired().HasMaxLength(150);
+                etd.Property(p => p.Description).IsRequired().HasMaxLength(250);
+            });
+        }
+
+        /// <summary>
+        /// Configurações da tabela Student
+        /// </summary>
+        public void ConfigDbStudent(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(etd =>
+            {
+                //Nome da tabela
+                etd.ToTable("tbStudent");
+                //Nome da coluna de id
+                etd.HasKey(p => p.StudentId).HasName("StudentId");
+                //Definição do tipo do campo e o autoincremento
+                etd.Property(p => p.StudentId).HasColumnType("int").ValueGeneratedOnAdd();
+                //Definição de campo not null e tamanho maximo
+                etd.Property(p => p.Name).IsRequired().HasMaxLength(150);
+                etd.Property(p => p.Email).IsRequired().HasMaxLength(250);
+                etd.Property(p => p.Text).IsRequired().HasMaxLength(250);
+                etd.Property(p => p.DateBirth).IsRequired().HasColumnType("date");
+            });
+        }
+
+        /// <summary>
+        /// Configurações da tabela StudentToCourse
+        /// </summary>
+        public void ConfigDbStudentToCourse(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<StudentToCourse>(etd =>
+            {
+                //Nome da tabela
+                etd.ToTable("tbStudentToCourse");
+                //Nome da coluna de id
+                etd.HasKey(p => p.StudentToCourseId).HasName("StudentToCourseId");
+                //Definição do tipo do campo e o autoincremento
+                etd.Property(p => p.StudentToCourseId).HasColumnType("int").ValueGeneratedOnAdd();
+                //Definição de relacionamento 1 para muitos (estudante -> StudentsToCourses)
+                etd.HasOne(p => p.Student).WithMany(p => p.StudentsToCourses).HasForeignKey(p => p.StudentId);
+                //Definição de relacionamento 1 para muitos (curso -> StudentsToCourses)
+                etd.HasOne(p => p.Course).WithMany(p => p.StudentsToCourses).HasForeignKey(p => p.CourseId);
+                //Coluna not null
+                etd.Property(p => p.CourseId).IsRequired();
+                etd.Property(p => p.StudentId).IsRequired();
+            });
+        }
+    }
+}
---
+++ SqlServerConnection/ClassLibrary/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Text { get; set; }
+        public DateTime DateBirth { get; set; }
+        public List<StudentToCourse> StudentsToCourses { get; set; }
+    }
+}

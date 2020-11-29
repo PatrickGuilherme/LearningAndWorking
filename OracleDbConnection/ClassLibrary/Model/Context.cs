b9f34e25fe06a8dcf375b886ba88494bb883d553
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2020-11-28 22:27:22 -0300

    Adição de projeto de conexão com oracle

---
+++ OracleDbConnection/ClassLibrary/Model/Context.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ClassLibrary
+{
+    public class Context : DbContext
+    {
+        //Atributos ligados ao banco de dados
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<StudentToCourse> StudentToCourse { get; set; }
+
+        /// <summary>
+        /// Inicia a criação do banco se não existir o banco
+        /// </summary>
+        public void StartConnection()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        /// <summary>
+        ///String de conexão
+        /// </summary>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Conexão utilizando sql server
+            optionsBuilder.UseOracle(
+                //Usuario da conexão (no oracle o usuario inicia com C##)
+                "User Id=NOME_USUARIO;" +
+                //Senha do banco
+                "Password=SENHA_USUARIO;" +
+                //Nome do banco de dados
+                "Data Source=localhost;");
+        }
+
+        /// <summary>
+        /// Iniciar Configurações das tabelas
+        /// </summary>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ConfigDbCourse(modelBuilder);
+            ConfigDbStudent(modelBuilder);
+            ConfigDbStudentToCourse(modelBuilder);
+        }
+
+        /// <summary>
+        /// Configurações da tabela Course
+        /// </summary>
+        public void ConfigDbCourse(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>(etd =>
+            {
+                //Nome da tabela
+                etd.ToTable("tbCourse");
+                //Nome da coluna de id
+                etd.HasKey(p => p.CourseId).HasName("CourseId");
+                //Definição do tipo do campo e o autoincremento
+                etd.Property(p => p.CourseId).HasColumnType("int").ValueGeneratedOnAdd();
+                //Definição de campo not null e tamanho maximo
+                etd.Property(p => p.Name).IsRequired().HasMaxLength(150);
+                etd.Property(p => p.Description).IsRequired().HasMaxLength(250);
+            });
+        }
+
+        /// <summary>
+        /// Configurações da tabela Student
+        /// </summary>
+        public void ConfigDbStudent(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(etd =>
+            {
+                //Nome da tabela
+                etd.ToTable("tbStudent");
+                //Nome da coluna de id
+                etd.HasKey(p => p.StudentId).HasName("StudentId");
+                //Definição do tipo do campo e o autoincremento
+                etd.Property(p => p.StudentId).HasColumnType("int").ValueGeneratedOnAdd();
+                //Definição de campo not null e tamanho maximo
+                etd.Property(p => p.Name).IsRequired().HasMaxLength(150);
+                etd.Property(p => p.Email).IsRequired().HasMaxLength(250);
+                etd.Property(p => p.Text).IsRequired().HasMaxLength(250);
+                etd.Property(p => p.DateBirth).IsRequired().HasColumnType("date");
+            });
+        }
+
+        /// <summary>
+        /// Configurações da tabela StudentToCourse
+        /// </summary>
+        public void ConfigDbStudentToCourse(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<StudentToCourse>(etd =>
+            {
+                //Nome da tabela
+                etd.ToTable("tbStudentToCourse");
+                //Nome da coluna de id
+                etd.HasKey(p => p.StudentToCourseId).HasName("StudentToCourseId");
+                //Definição do tipo do campo e o autoincremento
+                etd.Property(p => p.StudentToCourseId).HasColumnType("int").ValueGeneratedOnAdd();
+                //Definição de relacionamento 1 para muitos (estudante -> StudentsToCourses)
+                etd.HasOne(p => p.Student).WithMany(p => p.StudentsToCourses).HasForeignKey(p => p.StudentId);
+                //Definição de relacionamento 1 para muitos (curso -> StudentsToCourses)
+                etd.HasOne(p => p.Course).WithMany(p => p.StudentsToCourses).HasForeignKey(p => p.CourseId);
+                //Coluna not null
+                etd.Property(p => p.CourseId).IsRequired();
+                etd.Property(p => p.StudentId).IsRequired();
+            });
+        }
+    }
+}
---
+++ OracleDbConnection/SqlServerConnection/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using ClassLibrary;
+
+namespace SqlServerConnection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Classe de conexão com o banco
+            Console.WriteLine("Iniciou projeto...");
+            Context Context = new Context();
+
+            //Iniciar conexão com o banco
+            Console.WriteLine("Iniciando conexão com banco...");
+            Context.StartConnection();
+
+            //Mocar dados no banco
+            StartDataDB(Context);
+        }
+
+        /// <summary>
+        /// Método para mocar dados no banco
+        /// </summary>
+        private static void StartDataDB(Context context)
+        {
+            //Mocando dados no banco do aluno 1
+            Console.WriteLine("Mocando dados do aluno 1...");
+            Student student1 = new Student();
+            student1.Name = "Aluno 1";
+            student1.Email = "aluno1@email.com";
+            student1.Text = "Texto do aluno 1";
+            student1.DateBirth = DateTime.Now;
+            context.Add(student1);
+
+            //Mocando dados no banco do aluno 2
+            Console.WriteLine("Mocando dados do aluno 2...");
+            Student student2 = new Student();
+            student2.Name = "Aluno 2";
+            student2.Email = "aluno2@email.com";
+            student2.Text = "Texto do aluno 2";
+            student2.DateBirth = DateTime.Now;
+            context.Add(student2);
+
+            //Mocando dados do curso 1
+            Console.WriteLine("Mocando dados do curso 1...");
+            Course course1 = new Course();
+            course1.Name = "Curso 1";
+            course1.Description = "Descrição do curso 1";
+            context.Add(course1);
+
+            //Mocando dados do curso 2
+            Console.WriteLine("Mocando dados do curso 2...");
+            Course course2 = new Course();
+            course2.Name = "Curso 2";
+            course2.Description = "Descrição do curso 2";
+            context.Add(course2);
+
+            //Enviando dados para o banco
+            Console.WriteLine("Enviando dados para banco...");
+            context.SaveChanges();
+
+            //Mocando relacionamento: curso 1 com estudante 2
+            Console.WriteLine("Mocando relacionamento: curso 1 com estudante 2...");
+            StudentToCourse studentToCourse = new StudentToCourse();
+            studentToCourse.CourseId = course1.CourseId;
+            studentToCourse.StudentId = student2.StudentId;
+            context.Add(studentToCourse);
+
+            //Enviando dados para o banco
+            Console.WriteLine("Enviando dados para banco...");
+            context.SaveChanges();
+        }
+    }
+}
